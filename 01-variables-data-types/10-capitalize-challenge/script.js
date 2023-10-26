9cc95ada1bca2540ef07942f5315f43868b4ece0
Raisa <r.irakoze@alustudent.com>
2023-10-26 02:50:20 +0100

    word challenge

---
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -0,0 +1,15 @@
+// Create a new string called "myNewString" that holds the value of "Developer", using the lowercase value from "myString"
+const myString = "developer";
+
+let myNewString;
+
+// Solution 1:
+myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
+// Solution 2:
+myNewString = myString[0].toUpperCase() + myString.substring(1);
+// Solution 3:
+myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+// Solution 4:
+myNewString = myString.charAt(0).toUpperCase();
+
+console.log(myNewString);
