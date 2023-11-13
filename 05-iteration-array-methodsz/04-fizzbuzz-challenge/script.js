d84e6a9938588f5c675a68690f162c0bbe07ffc7
Raisa <r.irakoze@alustudent.com>
2023-11-13 18:45:30 +0200

    loop challenge

---
+++ 05-iteration-array-methodsz/04-fizzbuzz-challenge/script.js
@@ -0,0 +1,34 @@
+// Instructions:
+//Print/log the numbers from 1 to 100
+//For multiples of three print "Fizz" instead of the number
+//For multiples of five print "Buzz"
+//For numbers which are multiples of both three and five print "FizzBuzz".
+
+for (i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// while loop
+let j = 1;
+
+while (j <= 100) {
+  if (j % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (j % 3 === 0) {
+    console.log("Fizz");
+  } else if (j % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(j);
+  }
+
+  j++;
+}
