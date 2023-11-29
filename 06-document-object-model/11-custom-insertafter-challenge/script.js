af9afa7658c86d445e5380f9a7711c8d87bba6c3
Raisa <r.irakoze@alustudent.com>
2023-11-29 17:29:52 +0200

    challenge

---
+++ 06-document-object-model/11-custom-insertafter-challenge/script.js
@@ -0,0 +1,15 @@
+// Instructions
+
+//You may think that since there is an insertBefore() method, there is also an insertAfter(), but there isn't. In this challenge, I want you to create a custom insertAfter() function. If you don't want to do it as a challenge, that's fine, just follow along.
+
+//The first param will be newEl which will be a new element that you create with document.createElement()
+//The second param will be existingEl which is an element in the DOM that you want to insert your new element after
+
+function insertAfter(newEl, existingEl) {
+  existingEl.parentElement.insertBefore(newEl, existingEl);
+}
+
+const li = document.createElement("li");
+li.textContent = "Insert Me After";
+const firstItem = document.querySelector("li:first-child");
+insertAfter(li, firstItem);
